532dd663d80bad8cb9e4c351d5a56b6f641105cc
lvling <lvling@kcmds.com>
2016-11-11 16:06:28 +0800

    第一次

---
+++ app/src/main/java/com/example/jxg/dmd_lite_doc/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.jxg.dmd_lite_doc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
